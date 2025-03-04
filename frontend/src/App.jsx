366d34e43243f56f96350f6639504d95d949cacc
joozery <136419786+joozery@users.noreply.github.com>
2025-03-04 08:55:16 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LiveChat from "./components/LiveChat/LiveChat";
 // 📌 Import หน้าเว็บไซต์หลัก
 import Home from "./pages/Home";
 import Store from "./pages/Store";
-import TopupRobux from "./pages/TopupRobux"; // ✅ เพิ่ม Topup Robux
+import TopupRobux from "./pages/TopupRobux.jsx";
 
 // 📌 Import หน้า Dashboard
 import Dashbord from "./components/Dashbord/Dashbord";
